c18cf321a10d77bc2aef2e67fc9b12bed793878b
Lokesh <lokeshwaran100@gmail.com>
2023-06-28 15:44:42 +0000

    Relocated web app

---
+++ concordium/karna/app/src/data.js
@@ -0,0 +1,22 @@
+export const campaigns = [
+  {
+    "id": "5184",
+    "name": "Save the Children",
+    "desc": "Support Save the Children's mission to provide life-saving aid and long-term support to children in need. Your donation will help ensure access to education, healthcare, and protection for vulnerable children around the world."
+  },
+  {
+    "id": "5179",
+    "name": "Red Cross",
+    "desc": "Join the Red Cross in their humanitarian efforts to assist communities affected by disasters and emergencies. Your contribution will help provide emergency relief, medical assistance, and support in times of crisis."
+  },
+  {
+    "id": "5190",
+    "name": "Feeding America",
+    "desc": "Fight hunger and food insecurity in the United States with Feeding America. Your donation will help provide meals, support food banks, and advocate for policies to alleviate hunger and poverty."
+  },
+  {
+    "id": "5192",
+    "name": "Habitat for Humanity",
+    "desc": "Build safe and affordable homes with Habitat for Humanity. Your support will provide housing solutions, promote community development, and empower families to break the cycle of poverty."
+  }
+]
